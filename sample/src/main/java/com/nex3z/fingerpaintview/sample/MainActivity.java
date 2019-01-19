66096f0e8a090e50b72a0bc0943ab13820fdc502
nex3z <litianxing9@gmail.com>
2019-01-20 01:46:54 +0800

    Migrate to AndroidX.

--- sample/src/main/java/com/nex3z/fingerpaintview/sample/MainActivity.java
+++ sample/src/main/java/com/nex3z/fingerpaintview/sample/MainActivity.java
@@ -1,11 +1,12 @@
 package com.nex3z.fingerpaintview.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.nex3z.fingerpaintview.FingerPaintView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
